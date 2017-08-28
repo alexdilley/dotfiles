2c022e141e36e66f969eca8867c443b0140864dc
Alex Dilley <alex.dilley@gmail.com>
2017-08-28 16:02:26 +0100

    Command-T: fuzzy file finder plugin for Vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@ mkdir -p ~/.config/git
 ~/.dotfiles/dotsync/bin/dotsync -L
 crontab ~/.dotfiles/crontab
 mv ~/.backup/.dotfiles/ssh/id_ed25519 ~/.ssh/ 2>/dev/null
+cd ~/.dotfiles/vim/bundle/command-t/ruby/command-t/ext/command-t && ruby extconf.rb && make
