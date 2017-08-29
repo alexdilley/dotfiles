ffcb23af84d4c1bf482cbea8053a35835cf222f0
Alex Dilley <alex.dilley@gmail.com>
2017-08-29 16:59:08 +0100

    Automated installation of Homebrew packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.config/git
 ~/.dotfiles/dotsync/bin/dotsync -L
+brew bundle --file=~/.dotfiles/Brewfile
 crontab ~/.dotfiles/crontab
 mv ~/.backup/.dotfiles/ssh/id_ed25519 ~/.ssh/ 2>/dev/null
